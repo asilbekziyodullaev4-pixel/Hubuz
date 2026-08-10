69acf1ee997723e52969b0366337d318311f73f9
asilbekziyodullaev4-pixel <asilbekziyodullaev4@gmail.com>
2026-08-11 00:44:30 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,14 +1,32 @@
+import logging
+import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram.contrib.middlewares.logging import LoggingMiddleware 
+from aiogram.utils.executor import start_webhook
+APL_TOKEN= 8260817989:AAFvm8-VBVDY o1KZdmjTliCc3c5dAFRf5W'
 
-API_TOKEN = '8260817989:AAFfvm8-VBVDYo1KZdmjTlCc3C75dAfRf5M'
+WEBHOOK_HOST =
+'https://hubuz.onrender.com" WEBHOOK_PATH =f (API_TOKEN)'
+WEBHOOK_URL = f"(WEBHOOK_HOST) [WEBHOOK_PATH)"
+WEBAPP_HOST ="0.0.0.0 WEBAPP PORT = R int(os.getenv('PORT, 8O)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+logging.basicConfig(level=lo gging.INFO)
+bot =
+Bot(token=APL_TOKEN) dp = Dispatcher(bot) dp.middleware.setup(Loggin gMiddleware())
+@dp.message
+handler(commands=['start", 'help'])
+async def
+send_welcome(message: types.Message): await
+message.reply("Salom! Men sizga UC sotib olishda yordam beruvchi botman.")
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply("Salom! Men sizga UC sotib olishda yordam beruvchi botman.")
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+async def on_startup(dp): await bot.set_webhook(W EBHOOK_URL)
+async def on_shutdown(dp): await
+bot.delete_webhook()
+if- name --' start_webhook( dispatcher=dp,
+main
+webhook_path=WEBHO OK_PATH,
+on_startup=on_startup, on_shutdown=on_shutd
+own,
+host=WEBAPP_HOST, port=WEBAPP_PORT,
+    )
+`
