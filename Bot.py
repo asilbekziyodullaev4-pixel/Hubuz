d56ef85706ea53260fe971467497209f3331a14b
asilbekziyodullaev4-pixel <asilbekziyodullaev4@gmail.com>
2026-08-11 17:52:25 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -77,11 +77,11 @@ async def on_shutdown(dispatcher):
 # =========================
 
 if __name__ == "__main__":
-    start_webhook(
+    start_webhook
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
-    )
+    
